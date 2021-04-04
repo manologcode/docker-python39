4d2e00647d607b4e02ec25dc9af156b562db6757
Manuel Gomez Mateo <creawww@gmail.com>
2021-04-04 10:49:11 +0200

    first commit

---
+++ runDocker.sh
@@ -0,0 +1,17 @@
+if [ ! -d "site-packages" ]; then
+  docker run -d --name=mypyapp manologcode/mypyal3.9 /bin/ash
+  sleep 2
+  docker cp mypyapp:/usr/local/lib/python3.9/site-packages ./site-packages
+  docker rm -f mypyapp
+fi
+
+
+docker run -it --rm \
+--name=mypyapp \
+-e FLASK_APP=app.py \
+-e FLASK_ENV=development \
+-p 5000:5000 \
+-v $PWD/site-packages:/usr/local/lib/python3.9/site-packages \
+-v $PWD/app:/app \
+manologcode/mypyal3.9 \
+/bin/ash
